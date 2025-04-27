5ce348b51cffb2f9b055cb2d9a350d935ac81a14
idudko222 <idudko222@gmail.com>
2025-04-27 19:44:20 +0300

    Добавление функции получения даты

--- property_extractor.py
+++ property_extractor.py
@@ -7,21 +7,43 @@ class PropertyExtractor:
     def __init__(self, html):
         self.soup = BeautifulSoup(html,'html.parser')
 
-    def get_price(self, html):
-        soup = BeautifulSoup(html, "html.parser")
-        price = 0
-
+    def get_price(self):
         try:
-            price_element = soup.select_one(loc.PRICE[1])
+            price_element = self.soup.select_one(loc.PRICE[1])
             if price_element:
-                price_text = price_element.text.strip()
-                price_text = price_text.replace("\xa0", "").replace("₽", "")
-                price = price_text
+                price_text = price_element.text.strip().replace("\xa0", "").replace("₽", "")
 
-            return price
+            return price_text
 
         except AttributeError as e:
-            print(f'Цена не найдена {html}: {e}')
+            print(f'Цена не найдена: {e}')
+
+    def get_date(self):
+        try:
+            date_element = self.soup.select_one(loc.DATE_PUBLIC[1])
+            if date_element:
+                date_text = date_element.text.strip()
+                if 'вчера' in date_text:
+                    return (datetime.today().date() - timedelta(days=1)).strftime("%Y-%m-%d")
+                elif 'сегодня' in date_text:
+                    return datetime.today().date()
+                else:
+                    month_replace = {
+                        "января": "01", "февраля": "02", "марта": "03", "апреля": "04",
+                        "мая": "05", "июня": "06", "июля": "07", "августа": "08",
+                        "сентября": "09", "октября": "10", "ноября": "11", "декабря": "12"
+                    }
+                    date_text = date_text.replace("·", "").strip()
+                    date_text = date_text.split(" в ")[0].strip()
+                    parts = date_text.split()
+                    if len(parts) == 2:
+                        day, month = parts
+                        month = month_replace.get(month.lower())
+                        if month:
+                            return f"{datetime.today().year}-{month}-{day}"
+        except Exception as e:
+            print(f"Ошибка при парсинге даты: {e}")
+        return None
 
 
 
